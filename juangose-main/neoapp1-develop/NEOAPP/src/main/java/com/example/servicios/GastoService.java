aec793fa05fcffa7343a1cebb98c36c4de1e3ba0
MANOLOXRT <manoloproxrt@gmail.com>
2026-04-13 08:35:57 -0500

    ok

--- juangose-main/neoapp1-develop/NEOAPP/src/main/java/com/example/servicios/GastoService.java
+++ juangose-main/neoapp1-develop/NEOAPP/src/main/java/com/example/servicios/GastoService.java
@@ -1,11 +1,13 @@
 package com.example.servicios;
 
+import com.example.NEOAPP.modelos.Gasto;
+import com.example.repositorio.GastoRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-import com.example.repositorio.GastoRepository;
-import com.example.NEOAPP.modelos.Gasto;
+import org.springframework.http.HttpStatus;
+
 import java.util.List;
 
 @Service
@@ -14,24 +16,24 @@ public class GastoService {
     @Autowired
     private GastoRepository repositorio;
 
-    //servicio para guardar un gasto
-    public boolean guardar_gasto(Gasto datosGasto) {
-        if (datosGasto.getDescripcion() == null || datosGasto.getDescripcion().isBlank() || datosGasto.getDescripcion().isEmpty()) {
-            throw new ResponseStatusException(
-                HttpStatus.BAD_REQUEST
-            );
-        }
-        
-        try {
-            repositorio.save(datosGasto);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+   public Gasto guardar_gasto(Gasto datosGasto){
+
+    if (datosGasto.getValor() <= 0) {
+        throw new ResponseStatusException(
+            HttpStatus.BAD_REQUEST,
+            "El monto del gasto debe ser mayor a 0"
+        );
     }
 
-    //servicio para listar todos los gastos en BD
-    public List<Gasto> listar_gastos() {
-        return repositorio.findAll();
+    if (datosGasto.getDescripcion() == null 
+        || datosGasto.getDescripcion().isBlank()) {
+
+        throw new ResponseStatusException(
+            HttpStatus.BAD_REQUEST,
+            "La descripcion es obligatoria"
+        );
     }
+
+    return repositorio.save(datosGasto);
+}
 }
